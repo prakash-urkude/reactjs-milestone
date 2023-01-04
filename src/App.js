bf0b71123db204f4a7509ad9fb4c0958f754dc47
prakash-urkude <prakashurkude42416@gmail.com>
2023-01-04 23:48:14 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Project1 from './components/project1/project1'
+import Todo from './components/project1/project1'
+
+const App = () => {
+  return (
+    <>
+    <Project1/>
+    </>
+  )
+}
+
+export default App
